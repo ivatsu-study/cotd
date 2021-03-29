31627f605fd39a90a870bce8f6aa1c41c1ca51ef
IvaTsu <ivan.tsud@gmail.com>
2021-03-29 14:00:13 +0300

    Clenup starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
 // @flow
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App(): any {
-	const text: string = "Learn React";
-	const number = "2";
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Edit <code>src/App.js</code> and save to reload.</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{text}
-				</a>
-			</header>
-		</div>
+		<div></div>
 	);
 }
 
